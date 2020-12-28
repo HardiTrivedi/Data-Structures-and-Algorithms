4b522e651591bb538a566ddb6e71de5753e7095e
HardiTrivedi <66108629+HardiTrivedi@users.noreply.github.com>
2020-12-28 18:24:32 +0530

    Add files via upload

---
+++ Pointer_To_A_Structure/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+int main()
+{
+    struct rectangle r;
+    r.length=10;
+    r.breadth=5;
+    cout<<"Area of rectangle r "<<r.length*r.breadth<<endl;
+
+    struct rectangle *p;
+    p=&r;
+    p->length=15;//changing length and breadth of r
+    p->breadth=10;
+    cout<<"Area of rectangle r after changing length and breadth "<<p->length*p->breadth<<endl;
+    return 0;
+}
